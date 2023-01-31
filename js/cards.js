75a05bb4c7b4531e4246614768a86b5f9b8bb4a1
danielmedtar <dmedinat.ar@gmail.com>
2023-01-31 11:39:53 -0300

    finishing styles

--- js/cards.js
+++ js/cards.js
@@ -9,7 +9,8 @@ let createCard = (notif) => {
             <div class="card-content">
                 <img src="${notifs.avatar}" alt="Avatar ${notifs.name}" title="Avatar ${notifs.name}"/>
                 <p class="name">${notifs.name}</p>
-                <p class="message">${notifs.message}</p>
+                <p class="message1">${notifs.message1} <span class"message2">${notifs.message2}</span></p>
+                <img src="${notifs.img}" class="kim-img">
             </div>            
             <p class="time-content">${notifs.time}</p>            
             <p class="border">${notifs.private}</p>
@@ -19,4 +20,5 @@ let createCard = (notif) => {
         )        
     }  
 }
+
 createCard(notif)
